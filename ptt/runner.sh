480ab4b5ec9c534d19b479aedb0dbedb872472c1
csro7788@gmail.com <cs3282645>
2020-08-29 12:01:07 +0800

    v1.0

---
+++ ptt/runner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PATH=/Library/Frameworks/Python.framework/Versions/3.6/bin/
+cd /Users/yushi.chen/Dropbox/ptt_tool/ptt
+/Library/Frameworks/Python.framework/Versions/3.6/bin/scrapy crawl ptt
